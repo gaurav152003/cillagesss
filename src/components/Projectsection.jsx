560cae201c3bf028460d109825b94d7018e11318
Gaurav200315 <117560512+Gaurav200315@users.noreply.github.com>
2026-07-09 19:04:26 +0530

    first commit

--- src/components/Projectsection.jsx
+++ src/components/Projectsection.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import ProjectCard from "./ProjectCard";
+import ProjectCard from "../components/Projectcard.jsx";
 
 import project1 from "../assets/township.jpg";
 import project2 from "../assets/neptune.jpg";
